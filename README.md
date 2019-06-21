76de38489b848dca933a7808546575c833a5a2b6
olegk <oleg.korolenko@gmail.com>
2019-06-21 09:58:21 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gilded-rose-kata
+My take on [gilded-rose kata](https://github.com/emilybache/GildedRose-Refactoring-Kata) exercise 
---
+++ src/main/scala/com/gildedrose/Item.scala
@@ -0,0 +1,8 @@
+package com.gildedrose
+
+/**
+ * Created by okorolenko on 2019-06-21.
+ */
+
+class Item(val name: String, var sellIn: Int, var quality: Int) {
+
