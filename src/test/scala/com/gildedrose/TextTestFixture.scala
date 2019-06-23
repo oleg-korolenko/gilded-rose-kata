b13186ce6e04462039caa19da2b90b5ea2387b87
olegk <oleg.korolenko@gmail.com>
2019-06-23 19:54:06 +0200

    foxed test fixture

--- src/test/scala/com/gildedrose/TextTestFixture.scala
+++ src/test/scala/com/gildedrose/TextTestFixture.scala
@@ -14,5 +14,28 @@ import com.gildedrose.domain.{
   */
 object TexttestFixture {
   def main(args: Array[String]): Unit = {
+    var items = Array[ItemWithQuality](
+      AverageItem("+5 Dexterity Vest", 10, 20),
+      AgedBrie(2, 0),
+      AverageItem("Elixir of the Mongoose", 5, 7),
+      SulfurasTheHand(0),
+      SulfurasTheHand(-1),
+      BackStagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      BackStagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      BackStagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      // this conjured item does not work properly yet
+      ConjuredItem("Conjured Mana Cake", 3, 6)
+    )
 
+    val days = if (args.length > 0) args(0).toInt + 1 else 2
+    for (i <- 0 until days) {
+      System.out.println("-------- day " + i + " --------")
+      System.out.println("name, sellIn, quality")
+      for (item <- items) {
+        System.out.println(item.name + ", " + item.sellIn + ", " + item.quality)
+      }
+      System.out.println()
+      items = GildedRose.updateQuality(items)
+    }
+  }
 }
